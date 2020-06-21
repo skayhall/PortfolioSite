8c0ba44f979bd8d5d50ba03c09120e0c2df262a0
Skay Hall <shanakayhall@gmail.com>
2020-06-20 22:27:30 -0400

    added comments

--- js/contactformval.js
+++ js/contactformval.js
@@ -1,7 +1,7 @@
 //validates the contact form 
 function validation() {
 
-
+    //grab form element inputs and store them in variables
     var contactname = document.getElementById("contactname").value;
     var contactemail = document.getElementById("contactemail").value;
     var contactmessage = document.getElementById("contactmessage").value;
@@ -11,7 +11,7 @@ function validation() {
     var errormessage;
 
 
-
+    //if all form fields are empty display....
     if (contactname == '' || contactemail == '' || contactmessage == '') {
 
         errormessage = "All fields need to be filled out.";
@@ -20,6 +20,7 @@ function validation() {
     
     }
     
+    //if the contact name field has less then 3 characters validate and display....
     if (contactname.length <3) {
 
         errormessage = "Your full name needs be atleast 3 characters. ";
@@ -28,6 +29,7 @@ function validation() {
 
     }   
     
+    //if the contact is numbers validate and display....
     if (!isNaN(contactname)) {
 
         errormessage = "Your full name should only have letters.";
@@ -36,6 +38,7 @@ function validation() {
 
     }
 
+    //if contact email isnt valid validate and display....
     if (contactemail.indexOf("@") == -1 || contactemail.length < 6) {
 
         errormessage = "Please enter a valid email.";
@@ -44,7 +47,7 @@ function validation() {
 
     }
 
-
+    //if the contact message length is more than 200 characters validate and display....
     if (contactmessage.length >200) {
 
         errormessage = "Your message can only be a max of 200 characters. Please shorten it. ";
